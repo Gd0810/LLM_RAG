30584932632a3024079538bc8409734877ce82e8
Gd0810 <zeefer2000@gmail.com>
2026-04-06 12:13:38 +0530

    update:langchain-text-splitters

--- ingest.py
+++ ingest.py
@@ -1,5 +1,5 @@
 from langchain_community.document_loaders import PyPDFLoader, DirectoryLoader, TextLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_ollama import OllamaEmbeddings
 from langchain_community.vectorstores import Chroma
 
